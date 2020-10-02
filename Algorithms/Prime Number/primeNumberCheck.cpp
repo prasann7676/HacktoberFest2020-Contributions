6f10fee8592f894391a58f5961c7c0ea0a88fcdb
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2020-10-02 23:47:44 +0530

    Update primeNumberCheck.cpp

--- Algorithms/Prime Number/primeNumberCheck.cpp
+++ Algorithms/Prime Number/primeNumberCheck.cpp
@@ -1,30 +1,51 @@
-#include <iostream>
-#include<bits/stdc++.h>
+#pragma GCC target ("avx2")
+//#pragma GCC optimize "trapv"
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+#define input(a,n) for(ll i1=0;i1<n;i1++)cin>>a[i1]
+#define ll long long
+#define pi 2 * acos(0.0)
+#define usll unordered_set<ll>
+#define sll set<ll>
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define umll unordered_map<ll,ll>
+#define S second
+#define sz size()
+#define all(v) v.begin(),v.end()
+#define Y cout<< "YES"<< "\n"
+#define N cout<< "NO"<< "\n"
+#define F first
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ld long double
+#define mod 1000000007
 using namespace std;
-
-int main() {
-    int i, n;
-    bool isPrime = true;
-
-    cout << "Enter a positive integer: ";
-    cin >> n;
-
-    // 0 and 1 are not prime numbers
-    if (n == 0 || n == 1) {
-        isPrime = false;
-    }
-    else {
-        for (i = 2; i*i <= n; ++i) {            //Time Complexity reduced from O(n) to O(sqrt(n))
-            if (n % i == 0) {
-                isPrime = false;
-                break;
-            }
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n,f=1;
+    cin>>n;
+    for(ll i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            f=0;
+            break;
         }
     }
-    if (isPrime)
-        cout << n << " is a prime number";
+    if(n==1)
+        f=0;
+    else if(n==2)
+        f=1;
+    if(f)
+        Y;
     else
-        cout << n << " is not a prime number";
-
+        N;
     return 0;
 }
